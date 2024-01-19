db4c9e6bb92d090ee2b22107bf46fd82867ab5c5
Dustin Thurston <dthurston1996@gmail.com>
2024-01-18 22:30:57 -0500

    ready to generate final image, before adding in threading

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,81 @@ mod material;
 use std::io::{stderr, Write};
 use std::rc::Rc;
 use rand::Rng;
-use vec::{Point3, Color};
+use vec::{Point3, Color, Vec3};
 use ray::Ray;
 use hit::{Hit, World};
 use sphere::Sphere;
 use camera::Camera;
-use material::{Lambertian, Metal};
-use crate::material::Dielectric;
+use material::{Lambertian, Metal, Dielectric};
+
+
+fn random_scene() -> World {
+    let mut rng = rand::thread_rng();
+    let mut world = World::new();
+
+    let ground_mat = Rc::new(Lambertian::new(Color::new(0.5, 0.5, 0.5)));
+    let ground_sphere = Sphere::new(Point3::new(0.0, -1000.0, 0.0), 1000.0, ground_mat);
+
+    world.push(Box::new(ground_sphere));
+
+    for a in -11..=11 {
+        for b in -11..=11 {
+            let choose_mat: f64 = rng.gen();
+            let center = Point3::new((a as f64) + rng.gen_range(0.0..0.9),
+                                     0.2,
+                                     (b as f64) + rng.gen_range(0.0..0.9));
+
+            if choose_mat < 0.8 {
+                // Diffuse
+                let albedo = Color::random(0.0..1.0) * Color::random(0.0..1.0);
+                let sphere_mat = Rc::new(Lambertian::new(albedo));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.push(Box::new(sphere));
+            } else if choose_mat < 0.95 {
+                //Metal
+                let albedo = Color::random(0.4..1.0);
+                let fuzz = rng.gen_range(0.0..0.5);
+                let sphere_mat = Rc::new(Metal::new(albedo, fuzz));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.push(Box::new(sphere));
+            }else {
+                // glass
+                let sphere_mat = Rc::new(Dielectric::new(1.5));
+                let sphere = Sphere::new(center, 0.2, sphere_mat);
+
+                world.push(Box::new(sphere));
+            }
+        }
+    }
+
+    let mat1 = Rc::new(Dielectric::new(1.5));
+    let mat2 = Rc::new(Lambertian::new(Color::new(0.4, 0.2, 0.1)));
+    let mat3 = Rc::new(Metal::new(Color::new(0.7, 0.6, 0.5), 0.0));
+
+    let sphere1 = Sphere::new(Point3::new(0.0, 1.0, 0.0), 1.0, mat1);
+    let sphere2 = Sphere::new(Point3::new(-4.0, 1.0, 0.0), 1.0, mat2);
+    let sphere3 = Sphere::new(Point3::new(4.0, 1.0, 0.0), 1.0, mat3);
+
+    world.push(Box::new(sphere1));
+    world.push(Box::new(sphere2));
+    world.push(Box::new(sphere3));
+
+    world
+}
 
 fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
     if depth <= 0 {
-        return Color::new(0.0,0.0,0.0)
+        return Color::new(0.0, 0.0, 0.0);
     }
 
     if let Some(rec) = world.hit(r, 0.001, f64::INFINITY) {
-
         if let Some((attenuation, scattered)) = rec.mat.scatter(r, &rec) {
             attenuation * ray_color(&scattered, world, depth - 1)
         } else {
             Color::new(0.0, 0.0, 0.0)
         }
-        // let target = rec.p + Vec3::random_in_hemisphere(rec.normal);
-        // let r = Ray::new(rec.p, target - rec.p);
-        // 0.5 * ray_color(&r, world, depth - 1)
     } else {
         let unit_direction = r.direction().normalized();
         let t = 0.5 * (unit_direction.y() + 1.0);
@@ -40,31 +92,30 @@ fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
 
 fn main() {
     // Image
-    const ASPECT_RATIO: f64 = 16.0 / 9.0;
-    const IMAGE_WIDTH: u64 = 256;
+    const ASPECT_RATIO: f64 = 3.0 / 2.0;
+    const IMAGE_WIDTH: u64 = 1200;
     const IMAGE_HEIGHT: u64 = ((IMAGE_WIDTH as f64) / ASPECT_RATIO) as u64;
-    const SAMPLES_PER_PIXEL: u64 = 100;
-    const MAX_DEPTH: u64 = 5;
+    const SAMPLES_PER_PIXEL: u64 = 500;
+    const MAX_DEPTH: u64 = 50;
 
     // World
-    let mut world = World::new();
-    let mat_ground = Rc::new(Lambertian::new(Color::new(0.8,0.8,0.0)));
-    let mat_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
-    let mat_left = Rc::new(Dielectric::new(1.5));
-    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
+    let world = random_scene();
 
-    let sphere_ground = Sphere::new(Point3::new(0.0,-100.5, -1.0), 100.0, mat_ground);
-    let sphere_center = Sphere::new(Point3::new(0.0,0.0, -1.0), 0.5, mat_center);
-    let sphere_left = Sphere::new(Point3::new(-1.0,0.0, -1.0), 0.5, mat_left);
-    let sphere_right = Sphere::new(Point3::new(1.0,0.0, -1.0), 0.5, mat_right);
-
-    world.push(Box::new(sphere_ground));
-    world.push(Box::new(sphere_center));
-    world.push(Box::new(sphere_left));
-    world.push(Box::new(sphere_right));
 
     // Camera
-    let camera = Camera::new();
+    let lookfrom = Point3::new(13.0, 2.0, 3.0);
+    let lookat = Point3::new(0.0, 0.0, 0.0);
+    let vup = Vec3::new(0.0, 1.0, 0.0);
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
+
+    let camera = Camera::new(lookfrom,
+                             lookat,
+                             vup,
+                             20.0,
+                             ASPECT_RATIO,
+                             aperture,
+                             dist_to_focus);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
@@ -76,15 +127,15 @@ fn main() {
         stderr().flush().unwrap();
 
         for i in 0..IMAGE_WIDTH {
-            let mut pixel_color = Color::new(0.0,0.0,0.0);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
             for _ in 0..SAMPLES_PER_PIXEL {
                 let random_u: f64 = rng.gen();
                 let random_v: f64 = rng.gen();
 
                 let u = ((i as f64) + random_u) / ((IMAGE_WIDTH - 1) as f64);
-                let v = ((j as f64) + random_v)/ ((IMAGE_HEIGHT - 1) as f64);
+                let v = ((j as f64) + random_v) / ((IMAGE_HEIGHT - 1) as f64);
 
-                let r = camera.get_ray(u,v);
+                let r = camera.get_ray(u, v);
                 pixel_color += ray_color(&r, &world, MAX_DEPTH);
             }
             println!("{}", pixel_color.format_color(SAMPLES_PER_PIXEL));
--- src/material.rs
+++ src/material.rs
@@ -1,3 +1,4 @@
+use rand::Rng;
 use super::vec::{Color, Vec3};
 use super::ray::Ray;
 use super::hit::HitRecord;
@@ -68,6 +69,11 @@ impl Dielectric {
             ir: index_of_refraction
         }
     }
+
+    fn reflectance(cosine: f64, ref_idx: f64) -> f64{
+        let r0 = ((1.0 - ref_idx) / (1.0 + ref_idx)).powi(2);
+        r0 + (1.0 - r0) * (1.0 - cosine).powi(5)
+    }
 }
 
 impl Scatter for Dielectric {
@@ -82,7 +88,11 @@ impl Scatter for Dielectric {
         let cos_theta = ((-1.0) * unit_direction).dot_product(rec.normal).min(1.0);
         let sin_theta = (1.0 - cos_theta.powi(2)).sqrt();
 
-        let direction = if refraction_ratio * sin_theta > 1.0 {
+        let mut rng = rand::thread_rng();
+        let cannot_refract = refraction_ratio * sin_theta > 1.0;
+        let will_refract = rng.gen::<f64>() < Self::reflectance(cos_theta, refraction_ratio);
+
+        let direction = if cannot_refract || will_refract {
             unit_direction.reflect(rec.normal)
         } else {
             unit_direction.refract(rec.normal, refraction_ratio)
